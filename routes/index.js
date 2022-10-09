33e5da99ff8834bfafdd0e66425ce1ef9f5ec666
Sunil Gawai <71868299+sunilgawai@users.noreply.github.com>
2022-10-09 13:09:36 +0530

    readme added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { homeController } from "../controllers";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 const router = express.Router();
 
 // Store Product Route.
 router.post('/', homeController.home);
 
 export default router;
+
